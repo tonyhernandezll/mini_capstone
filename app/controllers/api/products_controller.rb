ba17fc1e58a6971b215233d0a866284c619c3d68
Tony <tonyhll1972@gmail.com>
2020-03-04 07:25:40 -0600

    add all and first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,11 @@
 class Api::ProductsController < ApplicationController
-  def product_method
-    @products = Product.first
-    render "my_product.json.jb"
+  def all_products
+    @products = Product.all
+    render "all_products.json.jb"
+  end
+
+  def first_product_method
+    @product = Product.first
+    render "first_product.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/sale_anything_url" => "products#product_method"
+    get "/all_products_url" => "products#all_products"
+    get "first_product_url" => "products#first_product_method"
   end
 end
