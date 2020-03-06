396178247fde5602f78ae5b8cc99391f009ee5c7
Tony <tonyhll1972@gmail.com>
2020-03-05 20:14:58 -0600

    added update method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,8 +4,29 @@ class Api::ProductsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    @product = Product.new(
+      name: params[:title],
+      price: params[:price],
+      image_url: params[:img],
+      description: params[:description],
+    )
+    @product.save
+    render "show.json.jb"
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render "show.json.jb"
   end
+
+  def update
+    @product = Product.find_by(:id params[:id])
+    @product.name = params[:input_name] || @product.name
+    @product.price = params[:input_price] || @product.price
+    @product.image = params[input_img] || @product.image_url
+    @product.description = params[input_description] || @product.description
+    render "show.json.jb"
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
+    post "/products" => "products#create"
     get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
   end
 end
